ee68eda74438ecc4588c8c5ea383a12f9bc73040
Ryuichi Ueda <ryuichiueda@gmail.fcom>
2016-10-01 17:02:00 +0900

    Add pip install em

--- travis_prepare.bash
+++ travis_prepare.bash
@@ -1,16 +1,24 @@
 #!/bin/bash -xve
 
+#required packages
+pip install catkin_pkg
+pip install em
+
+#ros install
 git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
 cd ./ros_setup_scripts_Ubuntu14.04_server
 bash ./step0.bash
 bash ./step1.bash
+
+#catkin setup
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
 source /opt/ros/indigo/setup.bash
 catkin_init_workspace
 cd ~/catkin_ws
-pip install catkin_pkg
 catkin_make
+
+#download own package
 cd ~/catkin_ws/src/
 git clone https://github.com/ryuichiueda/raspimouse_ros.git
 cd ~/catkin_ws
