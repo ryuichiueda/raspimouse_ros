fa9018135b579321b120aa23eb2be81cd7d39514
Ryuichi Ueda <ryuichiueda@gmail.fcom>
2016-10-01 20:44:46 +0900

    Add a lightsensor test

--- test/travis_test.bash
+++ test/travis_test.bash
@@ -1,9 +1,7 @@
 #!/bin/bash -xve
 
 roslaunch raspimouse_ros raspimouse.launch &
-
 sleep 20
 
-#rosrun raspimouse_ros check_driver_io.py
-
-rostopic echo /raspimouse/lightsensors -n 1
+rostopic echo /raspimouse/lightsensors -n 1 |
+diff - ./test/lightsensors_output
