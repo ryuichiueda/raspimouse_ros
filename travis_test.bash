ab591260aceadb79289204a62a22898a6975ede1
Ryuichi Ueda <ryuichiueda@gmail.fcom>
2016-10-01 16:32:29 +0900

    Move commands to .yml

--- travis_test.bash
+++ travis_test.bash
@@ -1,18 +1,5 @@
 #!/bin/bash -xve
 
-git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
-cd ./ros_setup_scripts_Ubuntu14.04_server
-bash ./step0.bash
-bash ./step1.bash
-
-source ~/.bashrc
-mkdir -p ~/catkin_ws/src/
-cd ~/catkin_ws/src
-PATH=$PATH:/opt/ros/indigo/bin/
-catkin_init_workspace
-cd ~/catkin_ws
-catkin_make
-source ~/catkin_ws/devel/setup.bash
 cd ~/catkin_ws/src/
 git clone https://github.com/ryuichiueda/raspimouse_ros.git
 roslaunch raspimouse_ros travis_test.launch
