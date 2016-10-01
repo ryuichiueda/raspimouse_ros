85782747e92c900d36bcfb51d84e335c0b539d46
Ryuichi Ueda <ryuichiueda@gmail.fcom>
2016-10-01 16:17:45 +0900

    Change to 14.04

--- travis_test.bash
+++ travis_test.bash
@@ -1,16 +1,17 @@
 #!/bin/bash -xve
 
-git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu16.04_server.git
-cd ./ros_setup_scripts_Ubuntu16.04_server
+git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
+cd ./ros_setup_scripts_Ubuntu14.04_server
 bash ./step0.bash
 bash ./step1.bash
 
-source ~/catkin_ws/devel/setup.bash
+source ~/.bashrc
 mkdir -p ~/catkin_ws/src/
 cd ~/catkin_ws/src
 catkin_init_workspace
 cd ~/catkin_ws
 catkin_make
+source ~/catkin_ws/devel/setup.bash
 cd ~/catkin_ws/src/
 git clone https://github.com/ryuichiueda/raspimouse_ros.git
 roslaunch raspimouse_ros travis_test.launch
