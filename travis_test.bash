9a5995731acdb95212d8dd227b49c12c61edc03e
Ryuichi Ueda <ryuichiueda@gmail.fcom>
2016-10-01 15:49:54 +0900

    Modift Travis files

---
+++ travis_test.bash
@@ -0,0 +1,16 @@
+#!/bin/bash -xve
+
+git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu16.04_server.git
+cd ./ros_setup_scripts_Ubuntu16.04_server
+bash ./step0.bash
+bash ./step1.bash
+
+source ~/.bashrc
+mkdir ~/catkin_ws/src/
+cd !$
+catkin_create_pkg raspimouse_ros std_msgs rospy
+cd ~/catkin_ws
+catkin_make
+cd ~/catkin_ws/src/
+git clone https://github.com/ryuichiueda/raspimouse_ros.git
+roslaunch raspimouse_ros raspimouse.launch
