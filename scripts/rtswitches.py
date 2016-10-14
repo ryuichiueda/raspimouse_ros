20ba9de84903000d49080f91bf9a4e82c3645e3e
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-10-14 15:04:23 +0000

    Add state transition

--- scripts/rtswitches.py
+++ scripts/rtswitches.py
@@ -3,31 +3,36 @@ import sys
 import rospy
 from raspimouse_ros.msg import Switches
 
-def talker():
-    devfile = '/dev/rtswitch'
+devfile = '/dev/rtswitch'
 
-    rospy.init_node('rtswitches')
-    pub = rospy.Publisher('switches', Switches, queue_size=10)
-    rate = rospy.Rate(10)
+rospy.init_node('rtswitches')
+pub = rospy.Publisher('switches', Switches, queue_size=10)
+rate = rospy.Rate(10)
 
-    d = Switches()
-    while not rospy.is_shutdown():
-        try:
-            with open(devfile + '0','r') as f:
-                d.front = True if '0' in f.readline() else False
-            with open(devfile + '1','r') as f:
-                d.center = True if '0' in f.readline() else False
-            with open(devfile + '2','r') as f:
-                d.rear = True if '0' in f.readline() else False
-        except:
-            rospy.logerr("cannot open " + devfile + "[0,1,2]")
+d = Switches()
 
-        pub.publish(d)
-        rate.sleep()
+d.state = 'neutral'
+state_change_counter = 0
 
-if __name__ == '__main__':
+while not rospy.is_shutdown():
     try:
-        talker()
+        with open(devfile + '0','r') as f:
+            d.front = True if '0' in f.readline() else False
+        with open(devfile + '1','r') as f:
+            d.center = True if '0' in f.readline() else False
+        with open(devfile + '2','r') as f:
+            d.rear = True if '0' in f.readline() else False
+    except:
+        rospy.logerr("cannot open " + devfile + "[0,1,2]")
 
-    except rospy.ROSInterruptException:
-        pass
+    if d.front: state_change_counter += 1
+
+    if state_change_counter >= 5 and not d.front:
+        state_change_counter = 0
+        if d.state == 'neutral': d.state = 'ready' 
+        elif d.state == 'ready': d.state = 'run' 
+        else:                    d.state = 'neutral'
+
+    pub.publish(d)
+    rate.sleep()
+
