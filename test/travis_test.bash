928dbdcb56ced1aafdd19bbbf61c958796197de7
Ryuichi Ueda <ryuichiueda@gmail.fcom>
2016-10-16 23:07:11 +0900

    Remove namespace

--- test/travis_test.bash
+++ test/travis_test.bash
@@ -4,18 +4,18 @@ roslaunch raspimouse_ros raspimouse.launch &
 sleep 20
 
 #publishing
-rostopic pub -1 /raspimouse/buzzer std_msgs/UInt16 -- 10
-rostopic pub -1 /raspimouse/motor_raw raspimouse_ros/MotorFreqs '123' '456'
+rostopic pub -1 /buzzer std_msgs/UInt16 -- 10
+rostopic pub -1 /motor_raw raspimouse_ros/MotorFreqs '123' '456'
 
 #service call
-rosservice call /raspimouse/put_motor_freqs 'left: -300
+rosservice call /put_motor_freqs 'left: -300
 right: 200
 duration: 1000'
-rosservice call /raspimouse/switch_motors "'on': true"
+rosservice call /switch_motors "'on': true"
 
 #listening test
-rostopic echo /raspimouse/lightsensors -n 1 > /tmp/test.lightsensors
-rostopic echo /raspimouse/switches -n 1 > /tmp/test.switches
+rostopic echo /lightsensors -n 1 > /tmp/test.lightsensors
+rostopic echo /switches -n 1 > /tmp/test.switches
 
 #output file test
 diff /tmp/test.lightsensors ./test/lightsensors_output
@@ -29,12 +29,12 @@ echo 1 | diff - /dev/rtmotoren0
 #switch state transition test
 echo 0 | sudo tee /dev/rtswitch0 && sleep 1
 echo 1 | sudo tee /dev/rtswitch0 && sleep 1
-rostopic echo /raspimouse/switches -n 1 | grep 'ready'
+rostopic echo /switches -n 1 | grep 'ready'
 
 echo 0 | sudo tee /dev/rtswitch0 && sleep 1
 echo 1 | sudo tee /dev/rtswitch0 && sleep 1
-rostopic echo /raspimouse/switches -n 1 | grep 'run'
+rostopic echo /switches -n 1 | grep 'run'
 
 echo 0 | sudo tee /dev/rtswitch0 && sleep 1
 echo 1 | sudo tee /dev/rtswitch0 && sleep 1
-rostopic echo /raspimouse/switches -n 1 | grep 'neutral'
+rostopic echo /switches -n 1 | grep 'neutral'
